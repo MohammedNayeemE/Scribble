5a5281fb86f20ae5ae28333f8d106cddefe0e8e5
MohammedNayeemE <zamanfiroz629@gmail.com>
2024-02-15 13:49:17 +0530

    fix : src.index.ts

--- Scribble/server/src/index.ts
+++ Scribble/server/src/index.ts
@@ -1,80 +1,80 @@
-// import express from 'express';
-// import { CLIENT_URL } from '../constant';
-// import cors from 'cors';
-// import { Auth } from '../routes';
-// import { errorHandler } from '../middleware/errorHandler';
-// import { Server } from 'socket.io';
+import express from 'express';
+import { CLIENT_URL } from '../constant';
+import cors from 'cors';
+import { Auth } from '../routes';
+import { errorHandler } from '../middleware/errorHandler';
+import { Server } from 'socket.io';
 
-// interface Room{
-//     [key : string] : string
-// }
+interface Room{
+    [key : string] : string
+}
 
-// const app = express();
-// const PORT = process.env.PORT || 8000;
-// app.use(cors(
-//    {
-//     origin : CLIENT_URL,
+const app = express();
+const PORT = process.env.PORT || 8000;
+app.use(cors(
+   {
+    origin : CLIENT_URL,
     
-//    } 
+   } 
 
-// ))
-// app.use(express.json());
-// app.use(errorHandler);
-// app.use(Auth.BASE_URL , Auth.router);
+))
+app.use(express.json());
+app.use(errorHandler);
+app.use(Auth.BASE_URL , Auth.router);
 
-// const server = app.listen(()=>{
-//     console.log(`server is running at port * ${PORT}`);
+const server = app.listen(()=>{
+    console.log(`server is running at port * ${PORT}`);
     
-// })
+})
 
-// const io = new Server(server , {
-//     cors : {
-//         origin : CLIENT_URL
-//     }
-// })
-// const rooms : Room  = {};
-// io.on('connection' , (socket) => {
+const io = new Server(server , {
+    cors : {
+        origin : CLIENT_URL
+    }
+})
+const rooms : Room  = {};
+io.on('connection' , (socket) => {
 
-//     socket.on('joinRoom' , (room) =>{
-//         socket.join(room);
-//         rooms[socket.id] = room;
+    socket.on('joinRoom' , (room) =>{
+        socket.join(room);
+        rooms[socket.id] = room;
 
-//         io.to(room).emit('userJoined' , socket.id);
+        io.to(room).emit('userJoined' , socket.id);
 
-//         socket.on('cursorMove', (position) => {
-//             io.to(rooms[socket.id]).emit('cursorMoved', { userId: socket.id, position });
+        socket.on('cursorMove', (position) => {
+            io.to(rooms[socket.id]).emit('cursorMoved', { userId: socket.id, position });
             
-//         });
+        });
 
-//     })
-//     socket.on('canvasImage' , (data) => {
-//         io.to(rooms[socket.id]).emit('canvasImage' , data);
+    })
+    socket.on('canvasImage' , (data) => {
+        io.to(rooms[socket.id]).emit('canvasImage' , data);
         
         
         
-//     })
+    })
 
-//     socket.on('sendMessage' , (message) =>{
+    socket.on('sendMessage' , (message) =>{
 
-//         io.to(rooms[socket.id]).emit('message', { userid: socket.id, chat : message });
-//         console.log(rooms); 
+        io.to(rooms[socket.id]).emit('message', { userid: socket.id, chat : message });
+        console.log(rooms); 
         
-//     });
+    });
     
 
 
-//     socket.on('disconnect', () => {
-//         const room = rooms[socket.id];
-//         if (room) {
-//             io.to(room).emit('userLeft', socket.id);
-//             //@ts-ignorets
-//             delete rooms[socket.id];
-//         }
-//         console.log(rooms);
-//     });                                                                                                                                                                         
+    socket.on('disconnect', () => {
+        const room = rooms[socket.id];
+        if (room) {
+            io.to(room).emit('userLeft', socket.id);
+            //@ts-ignorets
+            delete rooms[socket.id];
+        }
+        console.log(rooms);
+    });                                                                                                                                                                         
 
 
-// })
+})
 
 
 
